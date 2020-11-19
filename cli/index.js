d07b0bff7fd3c7d18409a2edf59b5907df0e6a69
K.C.Ashish Kumar <ashish-kumar.kothapally-chandrakanth@broadcom.com>
2020-11-19 12:55:13 +0530

    Release 2.0.0

---
+++ cli/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/**
+ * Retrieve System Information
+ * Author: KCAK11 (https://www.kcak11.com)
+ */
+
+var info = require("../exec");
+var result = "\n\n~~~~~~~~~~~~~~~~~~~~~~\nSystem Information\n~~~~~~~~~~~~~~~~~~~~~~\n\n";
+result += info.text;
+
+setTimeout(function () {
+    console.clear();
+    console.log(result);
+}, 100);
--- exec/index.js
+++ exec/index.js
@@ -1,15 +1,23 @@
-#!/usr/bin/env node
-
 /**
- * Retrieve System Information
+ * Retrieve System Information 
  * Author: KCAK11 (https://www.kcak11.com)
  */
 
-var info = require("../index");
-var result = "\n\n~~~~~~~~~~~~~~~~~~~~~~\nSystem Information\n~~~~~~~~~~~~~~~~~~~~~~\n\n";
-result += info.text;
+var os = require("os");
+var info = {};
+var text = "";
+for (var key in os) {
+    if (typeof os[key] === "function") {
+        try {
+            var outputInfo = JSON.stringify(os[key]());
+            var outputText = JSON.stringify(os[key](), null, 4);
+            info[key] = outputInfo;
+            text += key + ":  " + outputText + "\n\n";
+        } catch (exjs) { }
+    }
+}
 
-setTimeout(function () {
-    console.clear();
-    console.log(result);
-}, 100);
+module.exports = {
+    text: text,
+    info: info
+};
